2d48d7a295619ee235329bc52697094566f854d1
Jin Guo <9352437+jin-guo@users.noreply.github.com>
2022-03-16 09:29:59 -0400

    Add code demo for M7.

---
+++ src/ca/mcgill/cs/swdesign/m7/Developer.java
@@ -0,0 +1,7 @@
+package ca.mcgill.cs.swdesign.m7;
+
+public class Developer extends Employee {
+    public Developer(String pName, Department pDepartment) {
+        super(pName, pDepartment);
+    }
+}
